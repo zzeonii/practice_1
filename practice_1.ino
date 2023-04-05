031d05912b1db59ea31b2c7eedc277ff4fa9fd22
zzeonii <129237950+zzeonii@users.noreply.github.com>
2023-04-05 10:49:17 +0900

    blink1() function

--- practice_1.ino
+++ practice_1.ino
@@ -1,12 +1,17 @@
 const int led = 6;
 
+void blink1() 
+{
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500);
+}
+
 void setup() {
   pinMode(led, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led, HIGH);
-  delay(500);
-  digitalWrite(led, LOW);
-  delay(500);
+  blink1();
 }
