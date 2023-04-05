97f6866146a769a35591d16324fa6845e257ecd6
zzeonii <129237950+zzeonii@users.noreply.github.com>
2023-04-05 11:04:54 +0900

    blink2() function

--- practice_1.ino
+++ practice_1.ino
@@ -1,4 +1,5 @@
 const int led = 6;
+const int count = 3;
 
 void blink1() 
 {
@@ -8,10 +9,25 @@ void blink1()
   delay(500);
 }
 
+void blink2(int count) 
+{
+  while(count > 0)
+  {
+    digitalWrite(led, HIGH);
+    delay(500);
+    digitalWrite(led, LOW);
+    delay(500);
+    count = count - 1; //count를 줄인다.
+    Serial.println(count);
+  }    
+}
+
 void setup() {
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
 }
 
 void loop() {
-  blink1();
+  //blink1();
+  blink2(count);
 }
